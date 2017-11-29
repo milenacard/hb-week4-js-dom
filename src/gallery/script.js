9ceddaf59fd08fcbf7e3f28a6e36e8b582731558
milenacard <mile.card195@gmail.com>
2017-11-29 16:58:35 -0500

    Organizing the folders of the project

--- src/gallery/script.js
+++ src/gallery/script.js
@@ -11,7 +11,7 @@ function showDivs(n) {
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+     x[i].style.display = "none";  //en el css
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex-1].style.display = "block";  //no
 }
