60901d881e43224b3d846eae56b4177977806eaf
milenacard <mile.card195@gmail.com>
2017-12-01 16:20:31 -0500

    implementing disabled arrows

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -1,10 +1,12 @@
 
  let slideIndex = 0;
+ const slides = document.querySelectorAll(".gallery__list-item"); 
+ const buttons_dots = document.querySelectorAll("gallery__dot-item");
  const button_arrow_left = document.querySelector(".gallery__button--left");
  const button_arrow_right = document.querySelector(".gallery__button--right");
+
  const button_dot = document.querySelector(".gallery__dot-button");
- 
- 
+
  showImages(slideIndex);
 
  button_arrow_left.addEventListener('click', function(){
@@ -15,21 +17,42 @@
   showImages(slideIndex + 1);
  });
 
+button_dot.addEventListener('click', function(){
+  showImages();
+});
+
 function showImages(index) {
   var i;
-  const slides = document.querySelectorAll(".gallery__list-item");
   const isPositive = index >= 0;
   const isLessThanLength = index < slides.length;
   const isDifferentThanCurrent = index !== slideIndex;
 
+  if(index + 1 == slides.length){
+    button_arrow_right.setAttribute('disabled','');
+  }else{
+    button_arrow_right.removeAttribute('disabled');
+  }
+
+  if(index == 0){
+    button_arrow_left.setAttribute('disabled','');
+  }else{
+    button_arrow_left.removeAttribute('disabled');
+  }
+  
   if (isPositive && isLessThanLength && isDifferentThanCurrent) {
   slides[slideIndex].classList.remove("gallery__list-item--selected");
+
+  //buttons_dots[index].classList.remove("gallery__dot-button--selected");
   slideIndex = index;
+
+  //buttons_dots[index].classList.add("gallery__dot-button--selected");
   slides[slideIndex].classList.add("gallery__list-item--selected");
   }
 }
 
+
 function dots(){
-  const buttons_dots = document.querySelectorAll("gallery__dot-item");
+  
 }
+
 
