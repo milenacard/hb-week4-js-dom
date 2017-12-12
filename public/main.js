044b3cb87aa3dc12c5c30e893c085e9482e57505
milenacard <mile.card195@gmail.com>
2017-12-12 12:44:26 -0500

    refactoring code. Ready Gallery

--- public/main.js
+++
@@ -1,8 +0,0 @@
-import Gallery from './gallery/gallery.js'
-import data from './gallery/galleryData.js'
-
-console.log(data)
-
-/* eslint-disable */
-new Gallery(document.querySelector('.gallery'), data)
-/* eslint-enable */
